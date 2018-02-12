614453d783cbed225097c8be0828b5f0f173ae3e
nanugantiep <nanuganti@ebsco.com>
2018-02-12 11:38:28 -0500

    adding user code

--- src/main/java/com/auth0/samples/authapi/Application.java
+++ src/main/java/com/auth0/samples/authapi/Application.java
@@ -2,6 +2,8 @@ package com.auth0.samples.authapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +11,9 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
